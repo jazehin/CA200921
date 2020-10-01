e8efadac9abc009b9e939bbc5349642a859d055b
Kiss Bence <kiss.bence@verebelyszki.hu>
2020-10-01 20:54:32 +0200

    rip vau

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace CA200921
 
         static void Main(string[] args)
         {
-            int[] tomb = new int[100]; //vau
+            int[] tomb = new int[100];
             //feltöltés
             for (int i = 0; i < tomb.Length; i++) {
                 tomb[i] = rnd.Next(4, 34) * 3;
